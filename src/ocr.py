abfe50ffa4b6f341e2a56c2b9e144ec53fadb311
poo_hee_chang <poo_hee_chang@aiap.sg>
2024-01-11 17:54:57 +0800

    OCR classed

--- src/ocr.py
+++ src/ocr.py
@@ -1,7 +1,9 @@
+import numpy as np
 from mmocr.apis import MMOCRInferencer
-ocr = MMOCRInferencer(det='MaskRCNN_IC15', rec='abinet_20e_st-an_mj')
-ocr('./data/pic.jpg', show=True, print_result=True)
 
+# ocr = MMOCRInferencer(det='MaskRCNN_IC15',
+#                       rec='svtr-base')
+# ocr('./data/pic.jpg', show=True, print_result=True)
 
 # import mmcv
 # import matplotlib.pyplot as plt
@@ -10,3 +12,21 @@ ocr('./data/pic.jpg', show=True, print_result=True)
 #
 # plt.imshow(mmcv.bgr2rgb(predicted_img))
 # plt.show()
+
+class OCR:
+    def __init__(self, det='MaskRCNN_IC15', rec='svtr-base'):
+        """
+        See https://mmocr.readthedocs.io/en/dev-1.x/modelzoo.html for model names
+        :param det: Detection model. default as 'MaskRCNN_IC15'
+        :param rec: Recognition model. default as 'svtr-base'
+        """
+        self.det = det
+        self.rec = rec
+        self.model = MMOCRInferencer(det=self.det, rec=self.rec)
+    def predict(self, image_input:np.ndarray):
+        """
+
+        :param image_input: Image in numpy array. It should be in BGR order.
+        :return: prediction dict
+        """
+        return self.model(image_input)
