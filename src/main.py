2ff118995e9be54f24e4cdefc51a70f1b8d0457a
cpoohee <cpoohee>
2024-01-13 02:49:32 +0800

    clean up main.py

--- src/main.py
+++ src/main.py
@@ -1,47 +1,17 @@
-import torch
-import mmcv
-import matplotlib.pyplot as plt
-from VideoFeeder import VideoFeeder
-from OCR import OCR
-from WordMatch import WordMatch
-from KeyInfor import KeyInfor
+import sys
+from PyQt5.QtWidgets import QApplication
+from MainWindow import MainWindow
 
 def main():
-    feeder = VideoFeeder()
-    feeder.set_video('./data/vid.mp4')
-    feeder.set_rotate(VideoFeeder.ROTATE_90_COUNTERCLOCKWISE)
+    # test_keyword()
+    app = QApplication(sys.argv)
 
-    print(feeder.frame_count)
-    print(feeder.fps)
+    # Create a Qt widget, which will be our window.
+    window = MainWindow()
+    window.show()
 
-    success, image = feeder.grab_frame(1)
-
-    ocr = OCR()
-
-    res = ocr.predict(image)
-
-    # print(res)
-
-    key_info = KeyInfor()
-    key_info.add_keyword('sp02')
-    key_info.add_keyword('PRbpm')
-
-    interest_word, interest_word_center, original_word = key_info.parse_ocr(res)
-
-    print(interest_word)
-    print(interest_word_center)
-    print(original_word)
-
-    # plt.imshow(mmcv.bgr2rgb(image))
-    # plt.show()
-
-    # word_match = WordMatch()
-    # word_match.add_keyword('sp02')
-    # word_match.add_keyword('PRbpm')
-    #
-    # print(word_match.match_to_keyword('spO2'))
-    # print(word_match.match_to_keyword('bpm'))
-
+    # Start the event loop.
+    app.exec()
 
 if __name__=="__main__":
     main()
