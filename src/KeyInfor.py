d1af1d03ba5b66ec72c9c289f158e8b45cf46187
cpoohee <cpoohee>
2024-01-13 17:22:22 +0800

    update polygon centroid algo

--- src/KeyInfor.py
+++ src/KeyInfor.py
@@ -37,8 +37,38 @@ class KeyInfor:
         return interest_word, interest_word_center, original_word, interest_word_score
 
     def polygon_center(self, poly_list):
-        listY = poly_list[1::2]
-        listX = poly_list[::2]
-        xbar = np.mean(listX)
-        ybar = np.mean(listY)
-        return int(xbar), int(ybar)
+        list_y = poly_list[1::2]
+        list_x = poly_list[::2]
+        cx, cy, _ = self.centroid_poly(list_x, list_y)
+        return int(cx), int(cy)
+
+    def centroid_poly(self, X, Y):
+        """https://en.wikipedia.org/wiki/Centroid#Of_a_polygon"""
+        N = len(X)
+        # minimal sanity check
+        if not (N == len(Y)):
+            raise ValueError('X and Y must be same length.')
+        elif N < 3:
+            raise ValueError('At least 3 vertices must be passed.')
+        sum_A, sum_Cx, sum_Cy = 0, 0, 0
+        last_iteration = N - 1
+        # from 0 to N-1
+        for i in range(N):
+            if i != last_iteration:
+                shoelace = X[i] * Y[i + 1] - X[i + 1] * Y[i]
+                sum_A += shoelace
+                sum_Cx += (X[i] + X[i + 1]) * shoelace
+                sum_Cy += (Y[i] + Y[i + 1]) * shoelace
+            else:
+                # N-1 case (last iteration): substitute i+1 -> 0
+                shoelace = X[i] * Y[0] - X[0] * Y[i]
+                sum_A += shoelace
+                sum_Cx += (X[i] + X[0]) * shoelace
+                sum_Cy += (Y[i] + Y[0]) * shoelace
+        A = 0.5 * sum_A
+        factor = 1 / (6 * A)
+        Cx = factor * sum_Cx
+        Cy = factor * sum_Cy
+        # returning abs of A is the only difference to
+        # the algo from above link
+        return Cx, Cy, abs(A)
